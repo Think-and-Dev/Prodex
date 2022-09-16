417804dde6d631f1d8f575f81ed0ad7fe4730147
Guillermo Tala <guille86598@gmail.com>
2022-09-16 19:39:53 -0300

    fix prodex contructor

--- contracts/Prodex.sol
+++ contracts/Prodex.sol
@@ -107,13 +107,13 @@ contract Prodex is IProde, Ownable {
     address _oracle,
     uint256 _ngoDonationPercentage,
     uint256 _maxEvents,
-    uint256 _minWinnerPoints,
-    uint256
+    uint256 _minWinnerPoints
   ) {
     require(_token != address(0), 'INVALID TOKEN ADDRESS');
     require(_ngo != address(0), 'INVALID NGO ADDRESS');
     require(_oracle != address(0), 'INVALID ORACLE ADDRESS');
-    require(minWinnerPoints > 0, 'INVALID MIN WINNER POINTS');
+    require(_minWinnerPoints > 0, 'INVALID MIN WINNER POINTS');
+
     maxEvents = _maxEvents;
     minWinnerPoints = _minWinnerPoints;
     token = _token;
